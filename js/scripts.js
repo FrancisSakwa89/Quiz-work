a5b05933a31cdf658389bab1a3210c9c48f80345
FrancisSakwa89 <sakwafrancis89@gmail.com>
2019-01-13 11:36:34 +0300

    styled app

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,55 @@
-//Bussinesss Logic Interface(back-end)
-var testResults = function(one, two ,three, four){
-return one + two + three + four;
+// BUSINESS LOGIC
+var sum = 0;
+function scoreCalculator(userValue) {
+  var names = ["js", "year", "framework","function","dom","link"];
+  names.forEach(function(name) {
+    var userValue = $("input:radio[name=" + name + "]:checked").val();
+    if(userValue === "10") {
+      sum+=10;
+    }
+  });
 }
-  //User Interface(front-end)
-  $(document).ready(function(){
-  $(".begin").click(function(){
-    $("#test").fadeToggle("slow");
-  });
-  $("form#test").submit(function(event){
-    var question1 = parseInt($("input:radio[name=question1]:checked").val());
-    var question2 = parseInt($("input:radio[name=question2]:checked").val());
-    var question3 = parseInt($("input:radio[name=question3]:checked").val());
-    var question4 = parseInt($("input:radio[name=question4]:checked").val());
 
-    var result = testResults(question1, question2 ,question3, question4);
 
-    $("#display").text("Your score is:" + result);
-
+// USER INTERFACE LOGIC
+$(function() {
+  $("#start-btn").click(function(event) {
     event.preventDefault();
-    $(".result").show();
-  });
-});
+    var userFirstName = $("#firstName").val();
+    var userSecondName = $("#secondName").val();
+    if(userFirstName.length === 0 || userSecondName.length === 0) {
+      $("#no-name").show();
+      $("#firstName").addClass("enter-name");
+      $("#secondName").addClass("enter-name");
+    } else {
+      $("#intro").hide();
+      $("#form1").show();
+    }
+  })
+  $("#arrow-right").click(function() {
+    $("#form1").hide();
+    $("#form2").show();
+  })
+  $("#arrow-left").click(function() {
+    $("#form1").show();
+    $("#form2").hide();
+  })
+  $("#submit").click(function(event) {
+    event.preventDefault();
+    $("#score").show();
+    $("#form2").hide();
+    scoreCalculator();
+    var userFirstName = $("#firstName").val();
+    var userSecondName = $("#secondName").val();
+    if(sum <= 30) {
+      $(".name-display").text(`Nice try ${userFirstName} ${userSecondName},`);
+    } else {
+      $(".name-display").text(`Congratulations ${userFirstName} ${userSecondName},`);
+    }
+    $(".scoreDisplay").text(sum + "/60");
+  })
+  $("#retake").click(function(event) {
+    event.preventDefault();
+    location.reload();
+  })
+})
